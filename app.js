729d8f20278db716f006c17abe627dbc84de8ee2
Chawathe, Lalit <lalit.chawathe@changehealthcare.com>
2019-04-27 00:48:21 -0500

    Improved EventEmitter extended as Logger class. Object-Oriented design in JavaScript.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const log_funct = require('./logger');  // import logger.js
+const Logger = require('./logger');  // import logger.js
+var logger = new Logger();
 
 console.log(module); // print content of this module
 
 const path = require('path'); // Path library
-log_funct('This file is : '+ __filename);
+logger.log('This file is : '+ __filename);
 var pathObj = path.parse(__filename); 
 console.log(pathObj);
 
@@ -11,9 +12,9 @@ const os = require('os');
 var os_platform = os.platform();
 var total_mem = os.totalmem();
 var free_mem = os.freemem();
-log_funct('Platform : '+ os_platform);
-log_funct('Total memory : '+ total_mem);
-log_funct('Free  memory : '+ free_mem);
+logger.log('Platform : '+ os_platform);
+logger.log('Total memory : '+ total_mem);
+logger.log('Free  memory : '+ free_mem);
 
 const fs = require('fs');
 
@@ -28,30 +29,7 @@ console.log(files);
 console.log('\nReading files Asynchronously >>>')
 fs.readdir('./', function(err, files) {
     if (err) 
-        log_funct(err)
+        logger.log(err)
     else 
         console.log(files);
 });
-
-const EventEmitter = require('events');
-var event_emmitter = new EventEmitter();
-
-const EVEN_NAME = 'messageLogged';
-
-// event_emmitter.addListener(EVEN_NAME, function(eventArgs) {
-//     console.log(`Event '${EVEN_NAME}' happened with args ${eventArgs}`);
-// });
-
-// ABOVE is same as FOLLOWING.
-// 1. on() ~ addListener()
-// 2. callback function ~ shorthand => operator
-event_emmitter.on(EVEN_NAME, (eventArgs) => {
-    var jsonStr = JSON.stringify(eventArgs);
-    console.log(`Event '${EVEN_NAME}' happened with args ${jsonStr}`);
-});
-
-function emit(eventArgs) {
-    event_emmitter.emit(EVEN_NAME, eventArgs);
-}
-
-emit({'event_id': '123', 'msg': 'logMe'});
